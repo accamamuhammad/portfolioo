697bbf9d14bae30e4afe8c2d61abb3c5975858ea
accamamuhammad <140168234+accamamuhammad@users.noreply.github.com>
2024-03-24 09:39:37 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      spacing: {
+        "custom-margin": "20px", // Add your custom margin values here
+      },
+      padding: {
+        "custom-padding": "15px", // Add your custom padding values here
+      },
+      colors: {
+        "custom-color": "#ff00ff", // Add your custom color values here
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
